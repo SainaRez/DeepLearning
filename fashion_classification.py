7daa8278850b17fc88876f7eb921003d3bb63f6a
Saina <saina.r6@gmail.com>
2019-03-17 16:15:41 -0400

    trains and evaluates the test data with three layers

--- fashion_classification.py
+++ fashion_classification.py
@@ -1,4 +1,4 @@
-import numpy as npy
+import numpy as np
 import tensorflow as tf
 from tensorflow import keras
 import matplotlib.pyplot as plt
@@ -27,10 +27,39 @@ def build_model(train_images, train_labels, test_images, test_labels):
         keras.layers.Dense(128, activation=tf.nn.relu),
         keras.layers.Dense(10, activation=tf.nn.softmax)
     ])
-    model.compile(optimizer='adam', loss='sparse_categorial_crossentropy', matrics=['accuracy'])
+    model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
     return model
 
+def plot_image(i, predictions_array, true_label, img):
+  predictions_array, true_label, img = predictions_array[i], true_label[i], img[i]
+  plt.grid(False)
+  plt.xticks([])
+  plt.yticks([])
+  
+  plt.imshow(img, cmap=plt.cm.binary)
 
+  predicted_label = np.argmax(predictions_array)
+  if predicted_label == true_label:
+    color = 'green'
+  else:
+    color = 'red'
+  
+  plt.xlabel("{} {:2.0f}% ({})".format(class_names[predicted_label],
+                                100*np.max(predictions_array),
+                                class_names[true_label]),
+                                color=color)
+
+def plot_value_array(i, predictions_array, true_label):
+  predictions_array, true_label = predictions_array[i], true_label[i]
+  plt.grid(False)
+  plt.xticks([])
+  plt.yticks([])
+  thisplot = plt.bar(range(10), predictions_array, color="#777777")
+  plt.ylim([0, 1]) 
+  predicted_label = np.argmax(predictions_array)
+ 
+  thisplot[predicted_label].set_color('red')
+  thisplot[true_label].set_color('blue')
 
 
 
@@ -42,23 +71,37 @@ if __name__ == "__main__":
     class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 
                 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
     
-    print train_images.shape
-    print train_labels.shape
+    print "training data size:", train_images.shape
+    print "training labels size:", train_labels.shape
+    
+    
+    """
     plt.figure()
     plt.imshow(train_images[0])
     plt.colorbar()
     plt.grid(False)
     plt.show()
+    """
 
     train_images, train_labels, test_images, test_labels = pre_process(train_images, train_labels, test_images, test_labels)
 
     model = build_model(train_images, train_labels, test_images, test_labels)
-    model.fit(train_images, train_labels, epochs=20)
+    model.fit(train_images, train_labels, epochs=1)
     test_loss, test_acc = model.evaluate(test_images, test_labels)
-    print("TEST LOSS:", test_loss, "TEST ACCURACY:", test_acc)
+    print "TEST LOSS:", test_loss, "TEST ACCURACY:", test_acc
 
-    """
     predictions = model.predict(test_images)
-    for image in range(len(predictions)):
-        np.argmax(image)
-    """
+
+    num_rows = 5
+    num_cols = 3
+    num_images = num_rows*num_cols
+    plt.figure(figsize=(2*2*num_cols, 2*num_rows))
+    for i in range(num_images):
+        plt.subplot(num_rows, 2*num_cols, 2*i+1)
+        plot_image(i, predictions, test_labels, test_images)
+        plt.subplot(num_rows, 2*num_cols, 2*i+2)
+        plot_value_array(i, predictions, test_labels)
+    plt.show()
+
+
+    
